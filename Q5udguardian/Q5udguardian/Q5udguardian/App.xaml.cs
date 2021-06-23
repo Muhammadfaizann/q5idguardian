6f22fc35325b0f537efad61fcc52943acb929843
Alan Flores <aflores@skypointcloud.com>
2021-06-23 14:51:27 +0000

    added AppCenter NuGet packages, setup AppCenter on app start

--- Q5udguardian/Q5udguardian/Q5udguardian/App.xaml.cs
+++ Q5udguardian/Q5udguardian/Q5udguardian/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +18,9 @@ namespace Q5udguardian
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=18817ac9-9501-42bc-8359-af125430a4ed;" +
+                  "ios=b3fbb93d-ba4c-4c60-a537-e26958692b2e",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
