2fb74af6b082ffb4e2353b7f4f7aa41b562a6713
Alan Flores <aflores@skypointcloud.com>
2021-06-23 13:56:00 +0000

    added initial project structure

---
+++ Q5udguardian/Q5udguardian/Q5udguardian/Views/LovedOnes.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Q5udguardian.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LovedOnes : ContentPage
+    {
+        public LovedOnes()
+        {
+            InitializeComponent();
+        }
+    }
+}
