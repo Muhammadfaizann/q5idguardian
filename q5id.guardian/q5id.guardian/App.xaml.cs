a9f9aa06a31ecf45a6ec20fbc4f050c6d62beda4
Mistry Hardik <mistryhardik.05@live.com>
2021-07-09 23:57:02 +0530

    Refactor the guardian app project structure Setup basic project settings

---
+++ q5id.guardian/q5id.guardian/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using q5id.guardian.Views;
+using Xamarin.Forms;
+
+namespace q5id.guardian
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ q5id.guardian/q5id.guardian/Views/LovedOnesPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace q5id.guardian.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LovedOnesPage : ContentPage
+    {
+        public LovedOnesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
