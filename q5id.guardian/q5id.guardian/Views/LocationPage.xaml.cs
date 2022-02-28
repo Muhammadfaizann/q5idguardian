68813edf0068529228e9688be6a0cb1b21235110
Clint <ClintL@skypointcloud.com>
2022-02-28 13:38:19 -0500

    -Added new viewmodel called   LocationViewModel for handling saving of Location Settings.  -Added new page called LocationPage with two   entries for Distance and Frequency. -Added a new label with a command bind for   ShowLocationCommand. -Added new command called ShowLocationCommand   under the HomeViewModel for navigating to Location Page.

---
+++ q5id.guardian/q5id.guardian/Views/LocationPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using q5id.guardian.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace q5id.guardian.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LocationPage : BasePage<LocationViewModel>
+    {
+        public LocationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
